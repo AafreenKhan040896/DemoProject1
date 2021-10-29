731e70ca4b0c2d9aa172e6dc0c027feb9690f4f7
AafreenKhan040896 <aafreen4896@outlook.com>
2021-10-29 13:46:50 +0530

    LAyoutBuilder moved back to previous working code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:desktop_layout_view2/pages/desktop_page.dart';
 import 'package:desktop_layout_view2/pages/mobile_page.dart';
-import 'package:desktop_layout_view2/widgets/candidate_form.dart';
-import 'package:desktop_layout_view2/widgets/settings.dart';
 import 'package:flutter/material.dart';
 
 
@@ -19,35 +17,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => (MediaQuery.of(context).size.width < 600 ? const MobilePage() : const DesktopPage()) ,
-        '/second': (context) => Settings(),
-        
-      },
-    
-    );
-  }
-}
-
-class LayoutWidget extends StatelessWidget {
-  const LayoutWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            if(constraints.maxWidth < 600){
-              return const MobilePage();
+      home: Scaffold(
+        body: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if(constraints.maxWidth < 600){
+                return const MobilePage();
+              }
+              else{
+                return const DesktopPage();
+              } 
             }
-            else{
-              return const DesktopPage();
-            } 
-          }
+          ),
         ),
       ),
     );
